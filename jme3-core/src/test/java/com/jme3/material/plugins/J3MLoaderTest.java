bc3677f03068a53710a2f5791472094c49e79d49
Davis Rollman <drollma@hotmail.com>
2026-04-07 20:54:43 -0400

    Replace deprecated Mockito matcher import

--- jme3-core/src/test/java/com/jme3/material/plugins/J3MLoaderTest.java
+++ jme3-core/src/test/java/com/jme3/material/plugins/J3MLoaderTest.java
@@ -19,7 +19,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
